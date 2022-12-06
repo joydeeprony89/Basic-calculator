70f9e2c5f9524b6f1bcc2d8db0d46950a68903ae
jd_786 <jaydeep_shil@certree.com>
2022-12-06 13:55:51 +0530

    udpated to easy understanding code

--- Basic calculator/Program.cs
+++ Basic calculator/Program.cs
@@ -3,59 +3,56 @@ using System.Collections.Generic;
 
 namespace Basic_calculator
 {
+  // https://www.youtube.com/watch?v=HUfUzA9Ekgo
   class Program
   {
     static void Main(string[] args)
     {
       Program p = new Program();
-      string s = "(1+(4+5+2)-3)+(6+8)";
+      string s = "-312+-1-(-3+1+(+8--1)-1)+-2"; // -312-(-1)-(-3+1+(-8-(-1))-1)-(-2) valid inout based on leetcode
       Console.WriteLine(p.Calculate(s));
     }
     public int Calculate(string s)
     {
       Stack<int> stack = new Stack<int>();
-      int result = 0;
-      int number = 0;
+      int sum = 0;
       int sign = 1;
       for (int i = 0; i < s.Length; i++)
       {
         char c = s[i];
         if (char.IsDigit(c))
         {
-          number = 10 * number + (c - '0');
-        }
-        else if (c == '+')
-        {
-          result += sign * number;
-          number = 0;
+          int number = 0;
+          while(i < s.Length && char.IsDigit(s[i]))
+          {
+            number = 10 * number + (s[i] - '0');
+            i++;
+          }
+          i--;
+          number = number * sign;
           sign = 1;
+          sum += number;
         }
         else if (c == '-')
         {
-          result += sign * number;
-          number = 0;
-          sign = -1;
+          sign *= -1;
         }
         else if (c == '(')
         {
           //we push the result first, then sign;
-          stack.Push(result);
+          stack.Push(sum);
           stack.Push(sign);
           //reset the sign and result for the value in the parenthesis
           sign = 1;
-          result = 0;
+          sum = 0;
         }
         else if (c == ')')
         {
-          result += sign * number;
-          number = 0;
-          result *= stack.Pop();    //stack.pop() is the sign before the parenthesis
-          result += stack.Pop();   //stack.pop() now is the result calculated before the parenthesis
-
+          sum *= stack.Pop();    //stack.pop() is the sign before the parenthesis
+          sum += stack.Pop();   //stack.pop() now is the result calculated before the parenthesis
         }
       }
-      if (number != 0) result += sign * number;
-      return result;
+      return sum;
     }
   }
 }
